48e2f97362bdfb0698423243dbebd7fc18ba4587
cheoljin408 <kcj409@naver.com>
2021-12-07 02:13:54 +0900

    4.5 게시글 수정 화면 만들기

--- out/production/resources/static/js/app/index.js
+++ out/production/resources/static/js/app/index.js
@@ -1,8 +1,13 @@
 var main = {
     init: function() {
         var _this = this;
+
         $('#btn-save').on('click', function() {
             _this.save();
+        });
+
+        $('#btn-update').on('click', function() {
+            _this.update();
         });
     },
     save: function() {
@@ -21,6 +26,27 @@ var main = {
         }).done(function() {
             alert('글이 등록되었습니다.');
             window.location.href = "/";
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update: function() {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
         }).fail(function(error) {
             alert(JSON.stringify(error));
         });
